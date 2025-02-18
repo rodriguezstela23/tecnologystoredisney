76b108bf3f5a9cb833219b362a08861647ed5133
rodriguezstela23 <rodriguezstela23@gmail.com>
2025-02-18 19:31:40 +0000

    script.js

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-<script>
-    document.getElementById("emailForm").addEventListener("submit", async function(event) {
-      event.preventDefault();
-      var email = document.getElementById("email").value;
+document.getElementById("emailForm").addEventListener("submit", async function(event) {
+    event.preventDefault();
+    var email = document.getElementById("email").value;
 
-      const response = await fetch("/.netlify/functions/getLastEmail", {
+    const response = await fetch("/.netlify/functions/getLastEmail", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email }),
-      });
-
-      const data = await response.json();
-
-      if (data.message && data.message.includes("Es necesario que verifiques la dirección de correo electrónico asociada a tu cuenta de MyDisney")) {
-        // Si se encuentra el código de Disney+
-        alert(data.message); // Muestra el mensaje con el código de Disney
-      } else if (data.link) {
-        // Si se encuentra el link de Netflix
-        window.location.href = data.link; // Redirige automáticamente
-      } else {
-        alert("No se encontró resultado para tu cuenta, vuelve a intentarlo nuevamente.");
-      }
     });
-  </script>
-</body>
-</html>
+
+    const data = await response.json();
+    
+    // Verificar si se encuentra un enlace de Netflix
+    if (data.link) {
+        window.location.href = data.link; // Redirige automáticamente
+    } 
+    // Verificar si se encuentra el código de Disney Plus
+    else if (data.message && data.message.startsWith("Disney Plus Código")) {
+        alert(data.message); // Muestra el código de Disney Plus
+    } 
+    else {
+        alert("No se encontró resultado para tu cuenta, vuelve a intentarlo nuevamente.");
+    }
+});
