f5312b3ba801bd56c367f749019c3c907fe53142
rodriguezstela23 <rodriguezstela23@gmail.com>
2025-02-18 15:37:43 +0000

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,12 @@ document.getElementById("emailForm").addEventListener("submit", async function(e
     });
     
     const data = await response.json();
-    if (data.link) {
+
+    if (data.message && data.message.includes("Tu código de Disney Plus")) {
+        // Si la respuesta contiene el código de Disney+
+        alert(data.message); // Muestra el mensaje con el código
+    } else if (data.link) {
+        // Si se encuentra el link (probablemente de Netflix)
         window.location.href = data.link; // Redirige automáticamente
     } else {
         alert("No se encontró resultado para tu cuenta, vuelve a intentarlo nuevamente.");
